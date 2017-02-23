ccd782aa9a98f98356a3a63aeb4d9cac1eabfa6b
Viktorov616 <viktorov616@gmail.com>
2017-02-23 16:51:31 +0300

    Add button to scroll up into manage challenge page. Add functional to reset notice message, then challenge form closed.

--- src/components/CustomChallengeForm.js
+++ src/components/CustomChallengeForm.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actionCreators from '../actions/actionCreators';
@@ -8,8 +8,26 @@ import Checkbox from './Checkbox';
 import Select from './Select';
 import Form from './Form';
 
-function CustomChallengeForm(props) {
-  function handleCreate() {
+class CustomChallengeForm extends Component {
+  static handleSubmit(e) {
+    e.preventDefault();
+
+    e.target.reset();
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.handleCreate = this.handleCreate.bind(this);
+    this.handleSetNotice = this.handleSetNotice.bind(this);
+  }
+
+  componentWillUnmount() {
+    this.handleSetNotice();
+  }
+
+  handleCreate() {
+    const { props } = this;
     const { selectedChords, selectedType } = props.customChallengeForm;
     const id = `${props.challenges.data.length + 1}`;
     let noticeMessage;
@@ -31,65 +49,62 @@ function CustomChallengeForm(props) {
     props.clearCustomChallengeCheckboxesValues('selectedChords');
   }
 
-  function handleSetNotice() {
-    props.setNoticeMessage();
+  handleSetNotice() {
+    this.props.setNoticeMessage();
   }
 
-  function handleSubmit(e) {
-    e.preventDefault();
+  render() {
+    const { props } = this;
+    const { challengeTypes, chords, noticeMessage } = props.customChallengeForm;
+    const noticeMessageClass = 'custom-challenge-form__notice';
+    const noticeMessageClassModed = (noticeMessage.mod.length === 0)
+      ? noticeMessageClass
+      : `${noticeMessageClass} ${noticeMessageClass}--${noticeMessage.mod}`;
+    const notice = (noticeMessage.value.length !== 0)
+     ? <p className={noticeMessageClassModed}>{ noticeMessage.value }</p>
+     : null;
 
-    e.target.reset();
-  }
-
-  const { challengeTypes, chords, noticeMessage } = props.customChallengeForm;
-  const noticeMessageClass = 'custom-challenge-form__notice';
-  const noticeMessageClassModed = (noticeMessage.mod.length === 0)
-    ? noticeMessageClass
-    : `${noticeMessageClass} ${noticeMessageClass}--${noticeMessage.mod}`;
-  const notice = (noticeMessage.value.length !== 0)
-   ? <p className={noticeMessageClassModed}>{ noticeMessage.value }</p>
-   : null;
-
-  return (
-    <Form
-      className={'custom-challenge-form'}
-      handleSubmit={handleSubmit}
-      toggleForm={props.toggleCustomChallengeForm}
-    >
-      <h1 className="custom-challenge-form__header">Create challenge</h1>
-      <div className="custom-challenge-form__section">
-        <h2 className="custom-challenge-form__section-header">Choose challenge type.</h2>
-        <Select
-          handleChange={props.updateCustomChallengeForm}
-          handleClick={handleSetNotice}
-          name={'selectedType'}
-          options={challengeTypes}
-        />
-      </div>
-      <div className="custom-challenge-form__section">
-        <h2 className="custom-challenge-form__section-header">Choose chords.</h2>
-        <div className="custom-challenge-form__chords">
-          { chords.map(chord => <Checkbox
-            key={`custom-${chord}`}
-            handleCheckboxActivated={props.customChallengeCheckboxActivated}
-            handleCheckboxDeactivated={props.customChallengeCheckboxDeactivated}
-            handleClick={handleSetNotice}
-            groupName={'selectedChords'}
-            id={`custom-${chord}`}
-            value={chord}
-          />)}
+    return (
+      <Form
+        className={'custom-challenge-form'}
+        handleSubmit={this.constructor.handleSubmit}
+        toggleForm={props.toggleCustomChallengeForm}
+      >
+        <h1 className="custom-challenge-form__header">Create challenge</h1>
+        <div className="custom-challenge-form__section">
+          <h2 className="custom-challenge-form__section-header">Choose challenge type.</h2>
+          <Select
+            handleChange={props.updateCustomChallengeForm}
+            handleClick={this.handleSetNotice}
+            name={'selectedType'}
+            options={challengeTypes}
+          />
         </div>
-      </div>
-      <Btn
-        handleClick={handleCreate}
-        text={'Create'}
-        mods={['big', 'no-margin']}
-      />
-      <div className="custom-challenge-form__notice-wrapper">
-        { notice }
-      </div>
-    </Form>
-  );
+        <div className="custom-challenge-form__section">
+          <h2 className="custom-challenge-form__section-header">Choose chords.</h2>
+          <div className="custom-challenge-form__chords">
+            { chords.map(chord => <Checkbox
+              key={`custom-${chord}`}
+              handleCheckboxActivated={props.customChallengeCheckboxActivated}
+              handleCheckboxDeactivated={props.customChallengeCheckboxDeactivated}
+              handleClick={this.handleSetNotice}
+              groupName={'selectedChords'}
+              id={`custom-${chord}`}
+              value={chord}
+            />)}
+          </div>
+        </div>
+        <Btn
+          handleClick={this.handleCreate}
+          text={'Create'}
+          mods={['big', 'no-margin']}
+        />
+        <div className="custom-challenge-form__notice-wrapper">
+          { notice }
+        </div>
+      </Form>
+    );
+  }
 }
 
 CustomChallengeForm.propTypes = {
@@ -101,9 +116,12 @@ CustomChallengeForm.propTypes = {
     challengeTypes: PropTypes.array.isRequired,
     chords: PropTypes.array.isRequired,
     noticeMessage: PropTypes.object.isRequired,
+    selectedChords: PropTypes.array.isRequired,
+    selectedType: PropTypes.string.isRequired,
   }).isRequired,
   customChallengeCheckboxActivated: PropTypes.func.isRequired,
   customChallengeCheckboxDeactivated: PropTypes.func.isRequired,
+  setNoticeMessage: PropTypes.func.isRequired,
   toggleCustomChallengeForm: PropTypes.func.isRequired,
   updateCustomChallengeForm: PropTypes.func.isRequired,
 };
--- src/reducers/manageChallenges.js
+++ src/reducers/manageChallenges.js
@@ -1,9 +1,14 @@
 const defaultState = {
+  displayBtnUp: false,
   displayCustomChallengeForm: false,
 };
 
 export default function manageChallenges(state = defaultState, action) {
   switch (action.type) {
+    case 'HIDE_BTN_UP':
+      return Object.assign({}, state, { displayBtnUp: false });
+    case 'SHOW_BTN_UP':
+      return Object.assign({}, state, { displayBtnUp: true });
     case 'TOGGLE_CUSTOM_CHALLENGE_FORM':
       return Object.assign({}, state, {
         displayCustomChallengeForm: !state.displayCustomChallengeForm,
