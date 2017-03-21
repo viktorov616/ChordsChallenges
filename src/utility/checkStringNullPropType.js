606fe19811b2f223d78e9cdd30f6fa5af4939547
Viktorov616 <viktorov616@gmail.com>
2017-03-21 15:22:09 +0300

    Add updated verions of builded project.

--- src/utility/checkStringNullPropType.js
+++
@@ -1,9 +0,0 @@
-export default function checkStringNullPropType(props, propName, componentName) {
-  const prop = props[propName];
-
-  if (prop !== null && typeof prop !== 'boolean') {
-    return new Error(`Invalid prop '${propName}' supplied to ${componentName}. Validation failed.`);
-  }
-
-  return null;
-}
